c3fcc3a99bad4b292cd80fc5cee0bf61d9f3fb99
Chris Born <chris@peltedvenom.com>
2018-03-30 09:48:23 -0700

    show validation errors on the race form fields

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def field_error_messages(object, property)
+    return unless object.errors.include? property
+    errors = object.errors.full_messages_for(property)
+    content_tag :div do
+      errors.collect { |msg| concat content_tag :p, msg, class: ['help', 'is-danger'] }
+    end
+  end
+
+  def field_error_class(object, property)
+    'is-danger' if object.errors.include? property
+  end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -2,7 +2,7 @@ class Race < ApplicationRecord
   extend FriendlyId
   friendly_id :slug_candidates, use: :slugged
 
-  validates :title, presence: true, length: { minimum: 8, maximum: 64 }
+  validates :title, length: { minimum: 8, maximum: 64 }
   validates :date, timeliness: { on_or_after: -> { Date.current }, type: :date }
   validates :time, timeliness: { between: '7:00am'...'10:00pm', type: :time }
 
