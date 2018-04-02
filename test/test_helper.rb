ce15df2577a32d582647c963eb342d4cb44c1b54
Chris Born <chris@peltedvenom.com>
2018-04-02 15:03:45 -0700

    change output location for simpleconv

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,7 +3,6 @@ ENV['RAILS_ENV'] ||= 'test'
 require 'simplecov'
 
 SimpleCov.start 'rails' do
-  coverage_dir File.join('tmp', 'coverage')
   add_filter do |source_file|
     source_file.lines.count < 5
   end
