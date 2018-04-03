8950e15d831cb46c6481da17f8a643ef787f315d
Chris Born <chris@peltedvenom.com>
2018-04-03 13:16:01 -0700

    put the validation in the correct dir

--- app/controllers/concerns/unique_entry_validator.rb
+++
@@ -1,9 +0,0 @@
-class UniqueEntryValidator < ActiveModel::Validator
-  def validate(record)
-    if Entry.find_by(race: record.race, racer: record.racer)
-      record.errors.add(:racer, "is already entered in this race")
-    else
-      true
-    end
-  end
-end
